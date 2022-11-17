d3dacf76152f4a74ca2045e360ac1e70f886a53d
Devildcl <117940570+Devildcl@users.noreply.github.com>
2022-11-17 17:55:18 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,47 @@
-var box
-function setup() {
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  //uncomment the code to add animation to ship 
+
+  shipImg1 = loadAnimation("ship-1.png");
+  shipImg1 = loadAnimation("ship-1.png");
+  shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
+}
+
+function setup(){
   createCanvas(400,400);
-  box= createSprite(16,20,60,60)
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
 }
 
-function draw() 
-{
-  background(30);
-  if(keyIsDown(RIGHT_ARROW)){
-    box.x=box.x+3
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
   }
-  if(keyIsDown(LEFT_ARROW)){
-    box.x=box.x-3
-  }
-  if(keyIsDown(DOWN_ARROW)){
-    box.y=box.y-3
-  }
-  if(keyIsDown(UP_ARROW)){
-    box.y=box.y+3
-  }
-drawSprites()
+
+ 
+  drawSprites();
 }
-
-
-
-
